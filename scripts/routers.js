68f87f910c9f33dbfe5f2c708b495682df8a57e0
ustyk1 <eugenia.ustyk@gmail.com>
2023-03-30 23:04:40 +0300

    fix path

--- scripts/routers.js
+++ scripts/routers.js
@@ -1,19 +1,19 @@
 import page from "//unpkg.com/page/page.mjs";
 
 page('/sign-in', () => {
-  window.location.href = '/pages/sign-in/sign-in.html'
+  window.location.href = './pages/sign-in/sign-in.html'
 });
 
 page('/sign-up', () => {
-  window.location.href = '/index.html'
+  window.location.href = './index.html'
 });
 
 page('/profile', () => {
-  window.location.href = '/pages/profile/profile.html'
+  window.location.href = './pages/profile/profile.html'
 });
 
 page('/reset', () => {
-  window.location.href = '/pages/reset-password/reset-password.html'
+  window.location.href = './pages/reset-password/reset-password.html'
 });
 
 page();
